29dcba85d431ca216748dedcf188e671b8dc06bb
ryroshan <roshanros13@gmail.com>
2020-10-17 23:03:55 +0530

    Updates

---
+++ precache-manifest.87be1e8903b576ee0ae950fd05257e1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95deadfb3df3c565f8b21aea0b2a804",
+    "url": "/greetingapp/index.html"
+  },
+  {
+    "revision": "244a1ef50f4e9f351061",
+    "url": "/greetingapp/static/css/main.8a295a33.chunk.css"
+  },
+  {
+    "revision": "452be2d1c1fa5b74bf05",
+    "url": "/greetingapp/static/js/2.855154c0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/greetingapp/static/js/2.855154c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "244a1ef50f4e9f351061",
+    "url": "/greetingapp/static/js/main.e787fff2.chunk.js"
+  },
+  {
+    "revision": "8fb44a2d38c0ef05599e",
+    "url": "/greetingapp/static/js/runtime-main.0665434e.js"
+  }
+]);
